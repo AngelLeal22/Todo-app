4094795170e4041b4024e62c87841c7d50b98acb
Angelleal22 <angelleal1996@gmail.com>
2025-09-23 10:44:52 -0400

    frist Part

---
+++ config.js
@@ -0,0 +1,13 @@
+const PAGE_URL = process.env.NODE_ENV === "production"
+//aqui va un link de render
+? "placeholder"
+: "http://localhost:3000";
+
+const MONGO_URI = process.env.NODE_ENV === "production"
+//aqui va un link de render
+? process.env.MONGO_URI_PROD
+: process.env.MONGO_URI_TEST
+
+
+
+module.exports = {PAGE_URL, MONGO_URI}
---
+++ controllers/users.js
@@ -0,0 +1,116 @@
+const usersRouter = require('express').Router();
+const User = require("../models/user")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const nodemailer = require("nodemailer");
+const {PAGE_URL}= require("../config");
+const { request } = require('express');
+
+//ENDPOINT
+
+//para crear un nuevo usuario
+usersRouter.post('/', async (req, res) =>{
+    const {name, email, password} = req.body;
+    console.log(req.body)
+    
+
+    //validacion a nivel de backend
+    if (!name || !email || !password) {
+        return res.status(400).json({ error: 'Todos los espacions son requeridos' });
+    }
+
+    //Encriptacion de contraseñas, antes de guardar con bcrypt
+
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+    const newUser = new User({
+        name,
+        email,
+        passwordHash,
+    })
+    // guardar el usuario en al base de datos con el metodo .save()
+    const savedUser = await newUser.save();
+    // para crear el token
+    const token = jwt.sign({ id: savedUser.id}, process.env.ACCESS_TOKEN_SECRET,{ expiresIn :"1m"} )
+    console.log(token)
+
+    //envio del correo para verificar con nodemailer
+    const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+//esto es con nodemailer para mandarle el correo de verificacion al usuario.
+
+(async () => {
+  try {
+    const info = await transporter.sendMail({
+      from: process.env.EMAIL_USER, // sender address
+      to: savedUser.email, // list of receivers
+      subject: "Verificacion de Usuario GamerGames", // Subject line
+      html: `<a href="${PAGE_URL}/verify/${savedUser.id}/${token}"> Verificar Correo</a>`, // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  } catch (err) {
+    console.error("Error while sending mail", err);
+  }
+})();
+
+
+return res.status(201).json( "Usuario creado. Por Favor verificar tu correo electronico ")
+
+
+});
+
+
+
+usersRouter.patch('/:id/:token', async (req, res) => {
+ 
+    try {
+        const token = req.params.token;
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        const id = decodedToken.id;
+        await User.findByIdAndUpdate(id, { verified: true });
+        
+        return response.sendStatus(200)
+    } catch (error) {
+        //Encontra el email del usuario
+        const id = req.params.id;
+        const { email } = await User.findById(id);
+        //Firmar el nuevo token
+        const token = jwt.sign({ id: id }, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: '1d'
+        });
+        
+        //Enviar el email
+        const transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
+            auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+            },
+        });   
+
+    await transporter.sendMail({
+        from: process.env.EMAIL_USER, // sender address
+        to: email, // list of receivers
+        subject: "Verificación de usuario", // Subject line
+        html: `<a href="${PAGE_URL}/verify/${id}/${token}">Verificar usuario</a>`, // html body
+    })
+
+        return res.status(400).json({ error: 'El link ya expiro. Se ha enviado un nuevo link de verificacion'})
+    }
+});
+
+
+
+module.exports = usersRouter;
