11dd9f55a6887a86478f417b76df7dc06c085c04
Angelleal22 <angelleal1996@gmail.com>
2025-09-27 22:44:09 -0400

    change in app.js

--- app.js
+++ app.js
@@ -30,10 +30,10 @@ app.use(cookieParser());
 //RUTAS FRONTEND
 // Rutas corregidas según la estructura real
 app.use('/', express.static(path.resolve('views', 'home')));
+app.use('/components', express.static(path.resolve('views','components')));
 app.use('/styles', express.static(path.resolve('views', 'styles')));
 app.use('/signup', express.static(path.resolve('views', 'signup')));
 app.use('/login', express.static(path.resolve('views', 'login')));
-app.use('/components', express.static(path.resolve('views','Components')));
 app.use('/img', express.static(path.resolve('img')));
 app.use("/verify/:id/:token",express.static(path.resolve("views", "verify")))
 
